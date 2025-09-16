4231884a8f48b33c62a2a99fc887dab2794dbc50
Jonatas296 <jonatassilvadamata@gmail.com>
2025-09-16 13:45:22 -0300

    Alteracao no Models/Card.php

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -3,10 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Card extends Model {
     //
+    use HasFactory;
+
+    // Adicione deck_id, front e back no fillable
+    protected $fillable = [
+        'deck_id',
+        'front',
+        'back',
+    ];
+
+    // Relacionamento com Deck
     public function deck() {
         return $this->belongsTo(Deck::class);
+    }
+
+    // Relacionamento com History
+    public function histories() {
+        return $this->hasMany(History::class);
     }
 }
